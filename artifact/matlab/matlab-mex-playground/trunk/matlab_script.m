f1dbf7f0e396d6e26b2faa1c9c7355fd1c85f57c
christoph.pickl <christoph.pickl@gmail.com>
2011-04-02 11:09:39 +0000

    matlab rulez

---
+++ artifact/matlab/matlab-mex-playground/trunk/matlab_script.m
@@ -0,0 +1,17 @@
+x = 42;
+result = mxInOut(x)
+
+% result = pnAdd(20, 22)
+
+
+%{
+if A > B
+   'greater'
+elseif A < B
+   'less'
+elseif A == B
+   'equal'
+else
+   error('Unexpected situation')
+end
+%}

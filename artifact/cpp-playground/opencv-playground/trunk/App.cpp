0c1b22335d890f378457a2c078b13bc47c238fc8
christoph.pickl <christoph.pickl@gmail.com>
2011-04-07 10:52:31 +0000

    initial import

---
+++ artifact/cpp-playground/opencv-playground/trunk/App.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// we want to be very specific about what we want to include #include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+// highgui contains imwrite function
+#include <opencv2/highgui/highgui.hpp>
+
+int main() {
+	printf("main()\n");
+	cv::Mat colorArray[3];
+
+	return 0;
+}

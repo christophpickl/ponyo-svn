9f2a6f46f45bc2624cd2101556dd90b9b568afd4
christoph.pickl <christoph.pickl@gmail.com>
2011-04-08 15:36:13 +0000

    fixing proper cmake file

--- artifact/openni/playground/trunk/playgroundx/main_create_product_nodes.cpp
+++ artifact/openni/playground/trunk/playgroundx/main_create_product_nodes.cpp
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 #include <XnCppWrapper.h>
 
+#define CHECK_RC(returnCode, actionDescription) \
+	printf(">> %s ...\n", actionDescription); \
+	if(returnCode != XN_STATUS_OK) { \
+		fprintf(stderr, "actionDescription: %s\nxnGetStatusString(returnCode): %s\n", actionDescription, xnGetStatusString(returnCode)); \
+	}
+
 int main() {
 	xn::Context context;
 	printf("main()\n");
+
+	CHECK_RC(context.Init(), "context.Init()");
+
+	CHECK_RC(context.Shutdown(), "context.Shutdown()");
 
 	return EXIT_SUCCESS;
 }

2487fdbfba292ee1f1f7a78fc934cf95d82a7692
christoph.pickl <christoph.pickl@gmail.com>
2011-04-09 16:16:19 +0000

    added uncaught exception handler

--- artifact/cpp-playground/jna-playground/trunk/java/src/jpn/PnOpenNIJava.java
+++ artifact/cpp-playground/jna-playground/trunk/java/src/jpn/PnOpenNIJava.java
@@ -3,6 +3,7 @@ package jpn;
 import com.sun.jna.Callback;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
+import com.sun.jna.Callback.UncaughtExceptionHandler;
 
 public class PnOpenNIJava {
 
@@ -23,10 +24,18 @@ public class PnOpenNIJava {
     
     public static void main(String[] args) {
     	System.out.println("main() START");
+    	Native.setCallbackExceptionHandler(new UncaughtExceptionHandler() {
+			@Override
+			public void uncaughtException(Callback callback, Throwable throwable) {
+				System.err.println("uncaughtException(callback="+callback+", throwable="+throwable+")");
+				throwable.printStackTrace();
+			}
+		});
     	
     	PnOpenNICpp.FoobarCallback callback = new PnOpenNICpp.FoobarCallback() {
 			@Override public void invoke(int someNumber) {
 				System.out.println("callback.invoke(someNumber=" + someNumber + ")");
+//				throw new RuntimeException("haha!");
 			}
     	};
     	CPP.addCallback(callback);

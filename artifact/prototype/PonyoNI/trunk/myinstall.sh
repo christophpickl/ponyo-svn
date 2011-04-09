2ec651d33fdad6f031c96d0aec6c7b68a5e24dda
christoph.pickl <christoph.pickl@gmail.com>
2011-04-09 16:55:40 +0000

    cmake enhancements

---
+++ artifact/prototype/PonyoNI/trunk/myinstall.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+CWD=`pwd`
+echo CWD: ${CWD}
+
+function exec {
+	echo ">> ${1}"
+	${1}
+	CHECK ?
+}
+
+CHECK() {
+	RETURN_CODE=$1
+	if [ ${RETURN_CODE} -ne 0 ]; then
+		echo FAIL!!! RETURN_CODE was: ${RETURN_CODE}
+		cd ${CWD}
+		exit 1 
+	fi
+}
+
+
+#TARGET=/usr/local
+TARGET=/pntmp/usr/local
+
+echo "Building and installing ponyo libs to ${TARGET}"
+
+exec "cmake -DCMAKE_INSTALL_PREFIX:PATH=${TARGET}"
+#cmake -DCMAKE_INSTALL_PREFIX:PATH=/pxtmp \
+#      -DCMAKE_INSTALL_NAME_DIR:STRING=@executable_path/../somelib .
+
+exec "make"
+
+exec "make install"
+
+echo
+echo "FINISHED!"
+
+
+exit 0

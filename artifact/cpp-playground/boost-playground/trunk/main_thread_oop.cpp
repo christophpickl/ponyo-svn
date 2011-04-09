3fd6e12b0937bfd8b34feb8c1d424e6421f2cfae
christoph.pickl <christoph.pickl@gmail.com>
2011-04-09 22:33:19 +0000

    now its really an OOP thread ;)

--- artifact/cpp-playground/boost-playground/trunk/main_thread_oop.cpp
+++ artifact/cpp-playground/boost-playground/trunk/main_thread_oop.cpp
@@ -5,27 +5,37 @@
 
 // see: http://antonym.org/2009/05/threading-with-boost---part-i-creating-threads.html
 
-bool shouldQuit = false;
 class Foo {
-public:
-	Foo() {
-		printf("new Foo()\n");
-	}
-	~Foo() {
-		printf("~Foo()\n");
-	}
-
-	void onThread() {
-		printf("Foo.onThread() START\n");
-		boost::posix_time::seconds workTime(1);
-
-		while(!shouldQuit) {
-			printf("Foo WORKING again!\n");
-			boost::this_thread::sleep(workTime);
+	public:
+		Foo() : shouldRun(true) {
+			printf("new Foo()\n");
 		}
-
-		printf("Foo.onThread() END\n");
-	}
+		~Foo() {
+			printf("~Foo()\n");
+		}
+		void doit() {
+			printf("Foo.doit() START\n");
+			this->updateThread = boost::thread(&Foo::onThread, this);
+			printf("Foo.doit() END\n");
+		}
+		void stopAndJoin() {
+			printf("Foo.onstopAndJoin() START\n");
+			this->shouldRun = false;
+			this->updateThread.join();
+			printf("Foo.onstopAndJoin() END\n");
+		}
+		void onThread() {
+			printf("Foo.onThread() START\n");
+			boost::posix_time::seconds workTime(1);
+			while(this->shouldRun) {
+				printf("Foo WORKING again!\n");
+				boost::this_thread::sleep(workTime);
+			}
+			printf("Foo.onThread() END\n");
+		}
+	private:
+		boost::thread updateThread;
+		bool shouldRun;
 };
 
 int main(int argc, char* argv[]) {
@@ -33,19 +43,15 @@ int main(int argc, char* argv[]) {
 
 	Foo* f = new Foo();
 	printf("main() ... spawning new thread\n");
-	boost::thread someThread(&Foo::onThread, f);
+	f->doit();
 
 	printf("main() sleeping\n");
-	boost::posix_time::seconds sleepTime(3);
+	boost::posix_time::seconds sleepTime(5);
 	boost::this_thread::sleep(sleepTime);
 	printf("main() aborting\n");
-	shouldQuit = true;
 
-	printf("main() ... someThread.join()\n");
-	someThread.join();
-
+	f->stopAndJoin();
 	delete f;
-
 	std::cout << "main() END" << std::endl;
 	return 0;
 }

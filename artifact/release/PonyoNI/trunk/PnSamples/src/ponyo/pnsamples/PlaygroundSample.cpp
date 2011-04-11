b4de2f45719606f227b4502d7fa12875f417d529
christoph.pickl <christoph.pickl@gmail.com>
2011-04-11 20:13:10 +0000

    multi exe build working

---
+++ artifact/release/PonyoNI/trunk/PnSamples/src/ponyo/pnsamples/PlaygroundSample.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ponyo/pnsamples/PlaygroundSample.hpp>
+
+namespace pn {
+PlaygroundSample::PlaygroundSample() {
+}
+
+PlaygroundSample::~PlaygroundSample() {
+}
+}
+
+int main() {
+	printf("PlaygroundSample main()\n");
+	return 0;
+}
---
+++ artifact/release/PonyoNI/trunk/PnSamples/src/ponyo/pnsamples/PlaygroundSample.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef PLAYGROUNDSAMPLE_HPP_
+#define PLAYGROUNDSAMPLE_HPP_
+
+namespace pn {
+class PlaygroundSample {
+public:
+	PlaygroundSample();
+	virtual ~PlaygroundSample();
+};
+}
+
+#endif // PLAYGROUNDSAMPLE_HPP_

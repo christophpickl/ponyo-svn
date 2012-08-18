082d8a5d138f52a9c5c9caf7fec7c54da01ea420
christoph.pickl <christoph.pickl@gmail.com>
2012-08-18 02:05:45 +0000

    added new mindmaps

---
+++ priv/wt_eme/misc/wt_content_ChumKiu.mm
@@ -0,0 +1,26 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1345243057296" ID="ID_868933020" MODIFIED="1345243065218" TEXT="Chum Kiu">
+<node CREATED="1345243107874" ID="ID_924202975" MODIFIED="1345243174767" POSITION="right" TEXT="4. Satz - Seittritt">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="up"/>
+</node>
+<node CREATED="1345243079851" ID="ID_861082865" MODIFIED="1345243177966" POSITION="right" TEXT="5. Noi-Bong">
+<icon BUILTIN="full-2"/>
+<icon BUILTIN="up"/>
+</node>
+<node CREATED="1345243123280" ID="ID_1642101643" MODIFIED="1345243153040" POSITION="right" TEXT="6. Gam-Sao">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1345243065844" ID="ID_1940956132" MODIFIED="1345243138482" POSITION="left" TEXT="1. Satz - Schneiden">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1345243087107" ID="ID_541575545" MODIFIED="1345243140825" POSITION="left" TEXT="2. Satz - Fauststoss">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1345243092515" ID="ID_730840076" MODIFIED="1345243171823" POSITION="left" TEXT="3. Satz - Wendungen">
+<icon BUILTIN="full-1"/>
+<icon BUILTIN="up"/>
+</node>
+</node>
+</map>
